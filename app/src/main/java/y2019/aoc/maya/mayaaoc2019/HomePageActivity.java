25c99752bc28cb46649a3bc3960ec12d04dd6dfe
mayahaddad <mayahaddad25@gmail.com>
2019-10-02 09:56:25 +0300

    fixed erros

--- app/src/main/java/y2019/aoc/maya/mayaaoc2019/HomePageActivity.java
+++ app/src/main/java/y2019/aoc/maya/mayaaoc2019/HomePageActivity.java
@@ -3,18 +3,37 @@ package y2019.aoc.maya.mayaaoc2019;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+    Button startShift;
+    long startTime, stopTime;
+    boolean isStarted = false;
+    TextView tvTotalTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        startShift = findViewById(R.id.buttonStartShift);
+        startShift.setOnClickListener(this);
+
+        tvTotalTime = findViewById(R.id.textViewShiftTime);
+
     }
 
 
@@ -24,7 +43,7 @@ public class HomePageActivity extends AppCompatActivity {
         return true;
     }
     public boolean onOptionsItemSelected(MenuItem item){
-        Intent goToNextActivity=new Intent(getApplicationContext(), LogInActivity.class );
+        Intent goToNextActivity;
 
         switch(item.getItemId()){
             case R.id.mywork:
@@ -73,5 +92,24 @@ public class HomePageActivity extends AppCompatActivity {
         return true;
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if(!isStarted) {
+            startTime = System.currentTimeMillis();
+            startShift.setText("Stop Shift");
+            isStarted = true;
+        }else{
+            stopTime = System.currentTimeMillis();
+            startShift.setText("Start Shift");
+            isStarted = false;
+
+            tvTotalTime.setText("Worked Minutes: " +((stopTime-startTime)/1000/60));
+
+
+        }
+
+    }
+
 }
 

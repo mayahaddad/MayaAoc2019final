d4f519890493ffd5e44f04e56164c4e046efb2d7
mayahaddad <mayahaddad25@gmail.com>
2019-11-05 12:45:21 +0200

    fixed erros

--- app/src/main/java/y2019/aoc/maya/mayaaoc2019/DateTimeActivity.java
+++ app/src/main/java/y2019/aoc/maya/mayaaoc2019/DateTimeActivity.java
@@ -26,8 +26,11 @@ import static y2019.aoc.maya.mayaaoc2019.R.id.DateButton;
 public class DateTimeActivity extends AppCompatActivity implements View.OnClickListener{
     ImageButton DateButton;
     ImageButton TimeButton;
+    ImageButton Time2Button;
     TextView TimeText;
     TextView DateText;
+    TextView Time2Text;
+
 
 
     @Override
@@ -37,12 +40,16 @@ public class DateTimeActivity extends AppCompatActivity implements View.OnClickL
 
        DateButton=findViewById(R.id.DateButton);
        TimeButton=findViewById(R.id.TimeButton);
+        Time2Button=findViewById(R.id.Time2Button);
         DateText=findViewById(R.id.DateText);
         TimeText=findViewById(R.id.TimeText);
 
-        DateButton.setOnClickListener((View.OnClickListener) this);
-        TimeButton.setOnClickListener((View.OnClickListener) this);
+        Time2Text=findViewById(R.id.Time2Text);
 
+
+        DateButton.setOnClickListener( this);
+        TimeButton.setOnClickListener( this);
+        Time2Button.setOnClickListener( this);
     }
 
 
@@ -59,20 +66,58 @@ public class DateTimeActivity extends AppCompatActivity implements View.OnClickL
             int mDay = c.get(Calendar.DAY_OF_MONTH);
 
 
-            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+            final DatePickerDialog datePickerDialog = new DatePickerDialog(this,
                     new DatePickerDialog.OnDateSetListener() {
 
                         @Override
                         public void onDateSet(DatePicker view, int year,
                                               int monthOfYear, int dayOfMonth) {
 
+
                             DateText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            //datePickerDialog.dismiss();
+
+
+
 
                         }
                     }, mYear, mMonth, mDay);
             datePickerDialog.show();
         }
+
         if (v == TimeButton) {
+
+           /* // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            int mHour = c.get(Calendar.HOUR_OF_DAY);
+            int mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            TimeText.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();*/
+            Calendar mcurrentTime = Calendar.getInstance();
+            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+            int minute = mcurrentTime.get(Calendar.MINUTE);
+            TimePickerDialog mTimePicker;
+            mTimePicker = new TimePickerDialog(DateTimeActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                    TimeText.setText( selectedHour + ":" + selectedMinute);
+                }
+            }, hour, minute, true);//Yes 24 hour time
+            mTimePicker.setTitle("Select Time");
+            mTimePicker.show();
+        }
+        if (v == Time2Button) {
 
             // Get Current Time
             final Calendar c = Calendar.getInstance();
@@ -87,7 +132,7 @@ public class DateTimeActivity extends AppCompatActivity implements View.OnClickL
                         public void onTimeSet(TimePicker view, int hourOfDay,
                                               int minute) {
 
-                            TimeText.setText(hourOfDay + ":" + minute);
+                            Time2Text.setText(hourOfDay + ":" + minute);
                         }
                     }, mHour, mMinute, false);
             timePickerDialog.show();

16a44358e9a978a4dc6505c8d9475c9377c236b6
mayahaddad <mayahaddad25@gmail.com>
2019-12-04 11:06:08 +0200

    fixed erros

--- app/src/main/java/y2019/aoc/maya/mayaaoc2019/LogInActivity.java
+++ app/src/main/java/y2019/aoc/maya/mayaaoc2019/LogInActivity.java
@@ -1,22 +1,35 @@
 package y2019.aoc.maya.mayaaoc2019;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private static final String TAG = "FIREBASE";
+    private FirebaseAuth mAuth;
     EditText editTextEmail, editTextPassword;
     Button buttonLogIn, buttonSignUp;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -42,10 +55,7 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                 Toast.makeText(this, "Empty username or password", Toast.LENGTH_LONG).show();
             }
             else{
-                Intent i =new Intent (this,MainActivity.class);
-                i.putExtra("email",editTextEmail.getText().toString());
-                startActivity(i);
-
+                signIn(editTextEmail.getText().toString(),editTextPassword.getText().toString());
 
             }
         }
@@ -53,5 +63,29 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
             Intent i=new Intent( this,SignUpActivity.class);
             startActivity(i);
         }
+    }
+    public void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //      updateUI(user);
+                            Intent i = new Intent(LogInActivity.this,MainActivity.class);
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 }

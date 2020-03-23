9b29dc7ee8af35f8bc094ba876ee86d8f5c8d301
mayahaddad <mayahaddad25@gmail.com>
2020-03-23 08:51:56 +0200

    fixed erros

--- app/src/main/java/y2019/aoc/maya/mayaaoc2019/MainActivity.java
+++ app/src/main/java/y2019/aoc/maya/mayaaoc2019/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         buttonNow = findViewById(R.id.buttonNow);
+        
         buttonNow.setOnClickListener(this);
 
         buttonMyShifts = findViewById(R.id.buttonMyShifts);
